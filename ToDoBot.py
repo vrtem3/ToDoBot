214dc1ffda455b007b35b8a5a4a97466b4c82b2d
Artem Gorbunov <artem@gorbu.nov.ru>
2022-04-13 23:43:10 +0500

    master

---
+++ ToDoBot.py
@@ -0,0 +1,60 @@
+HELP = """
+***
+
+help - вывести справку
+add - добавить новую задачу
+show - вывести все добавленные задачи на конкретную дату
+dates - вывести даты, на которые есть задачи
+tasks - вывести все задачи
+task - добавить быструю задачу
+
+***"""
+
+tasks = {
+
+}
+
+def add_task(date, task):
+    if date in tasks:  #  Если дата есть в словаре, то добавляем задачу по ключу даты
+        tasks[date].append(task)
+    else:  # Если даты нет в словаре, то создаем дату и добавляем задачу по ключу даты
+        tasks[date] = []
+        tasks[date].append(task)
+    print(f'Задача "{task}" добавлена на дату: {date}')
+
+run = True
+
+while run:
+    print(HELP)
+    command = input("Введите команду: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        date = input("Введите дату для отображения списка задач: ")
+        if date in tasks:
+            for task in tasks[date]:
+                print('- ', task)
+        else:
+            print("Задач на эту дату нет.")
+    elif command == "add":
+        date = input("Введите дату для добавления задачи: ")
+        task = input("Введите название задачи: ")
+        add_task(date, task)
+    elif command == "dates":
+        if len(tasks) > 0:
+            for date in tasks.keys(): 
+                print(date)
+        else:
+            print("Нет запланированных задач.")
+    elif command == "tasks":
+        if len(tasks) > 0:
+            for key, value in tasks.items():
+                print(f'{key}: {value}')
+        else:
+            print("Нет запланированных задач.")
+    elif command == "exit":
+        print("Программа остановлена.")
+        run = False
+    else: 
+        print("Неизвестная команда! Введите доступную команду!")
+
